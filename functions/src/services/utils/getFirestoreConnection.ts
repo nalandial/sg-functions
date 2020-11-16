878b4554739056c09a32215c7726d8e6ccd72fce
Alex Beardsley <shadowphoenix@gmail.com>
2020-11-15 22:56:34 -0500

    quick comment update

--- functions/src/services/utils/getFirestoreConnection.ts
+++ functions/src/services/utils/getFirestoreConnection.ts
@@ -1,6 +1,6 @@
 import * as admin from 'firebase-admin';
 
-// TODO: not sure how i feel about this being a global variable. singleton seems kinda weird for JS
+// TODO: not sure how i feel about this being a global variable vs. a singleton - singleton seems kinda weird for JS
 let firestoreConnection : FirebaseFirestore.Firestore;
 
 export const getFirestoreConnection = () : FirebaseFirestore.Firestore => {
